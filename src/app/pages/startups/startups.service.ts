1236ef3b8af707aa27c8ae266b94f2fcb8513842
Raj Desai <rddesai@us.ibm.com>
2016-11-04 15:35:35 -0700

    Added search function.

--- src/app/pages/startups/startups.service.ts
+++ src/app/pages/startups/startups.service.ts
@@ -13,7 +13,31 @@ export class StartupsService {
       username: '@mdo',
       email: 'mdo@gmail.com',
       age: '28'
-    }
+    },
+    {
+      id: 2,
+      firstName: 'Jacob',
+      lastName: 'Thornton',
+      username: '@fat',
+      email: 'fat@yandex.ru',
+      age: '45'
+    },
+    {
+      id: 3,
+      firstName: 'Larry',
+      lastName: 'Bird',
+      username: '@twitter',
+      email: 'twitter@outlook.com',
+      age: '18'
+    },
+    {
+      id: 4,
+      firstName: 'John',
+      lastName: 'Snow',
+      username: '@snow',
+      email: 'snow@gmail.com',
+      age: '20'
+    }    
     ];
   
   getData(): Promise<any> {
